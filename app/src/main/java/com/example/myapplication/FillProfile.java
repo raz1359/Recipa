860273421294323cb97e2dd0fd708a77a99644c5
raz1359 <razcraft1359@gmail.com>
2023-03-21 16:01:16 +0200

    user database

--- app/src/main/java/com/example/myapplication/FillProfile.java
+++ app/src/main/java/com/example/myapplication/FillProfile.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,10 +11,19 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
 public class FillProfile extends AppCompatActivity implements View.OnClickListener {
 
+    public Button button;
     public ImageView imageView;
     public EditText fullName, nickname;
+
+    FirebaseDatabase db = FirebaseDatabase.getInstance();
+    DatabaseReference reference =db.getReference().child("Users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +38,25 @@ public class FillProfile extends AppCompatActivity implements View.OnClickListen
 
         nickname = findViewById(R.id.etnickname);
         nickname.setOnClickListener(this);
+
+        button = findViewById(R.id.btnContinue);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String tempName = fullName.getText().toString();
+                String tempNickname = nickname.getText().toString();
+
+                HashMap<String, String> usermap = new HashMap<>();
+                usermap.put("name", tempName);
+                usermap.put("nickname", tempNickname);
+
+                reference.push().setValue(usermap);
+            }
+        });
     }
+
 
     @Override
     public void onClick(View view) {
@@ -36,7 +64,6 @@ public class FillProfile extends AppCompatActivity implements View.OnClickListen
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
 
-        String tempName = fullName.getText().toString().trim();
-        String tempNickname = nickname.getText().toString().trim();
+
     }
 }
