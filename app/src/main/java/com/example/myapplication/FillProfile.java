268753981de530efd21df5ccd5c87d1205f14aa5
raz1359 <razcraft1359@gmail.com>
2023-06-16 19:54:51 +0300

    Fixed spelling mistakes

--- app/src/main/java/com/example/myapplication/FillProfile.java
+++ app/src/main/java/com/example/myapplication/FillProfile.java
@@ -4,13 +4,17 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -18,12 +22,16 @@ import java.util.HashMap;
 
 public class FillProfile extends AppCompatActivity implements View.OnClickListener {
 
+    public static final String TAG = "Raz";
     public Button button;
     public ImageView imageView;
     public EditText fullName, nickname;
+    public String uID;
 
-    FirebaseDatabase db = FirebaseDatabase.getInstance();
-    DatabaseReference reference =db.getReference().child("Users");
+    FirebaseDatabase db;
+    DatabaseReference reference;
+    FirebaseAuth mAuth;
+    FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,13 +56,17 @@ public class FillProfile extends AppCompatActivity implements View.OnClickListen
                 String tempName = fullName.getText().toString();
                 String tempNickname = nickname.getText().toString();
 
-                HashMap<String, String> usermap = new HashMap<>();
-                usermap.put("name", tempName);
-                usermap.put("nickname", tempNickname);
-
-                reference.push().setValue(usermap);
+                reference.setValue(tempName);
             }
         });
+
+        // Firebase initi
+        db = FirebaseDatabase.getInstance("https://recipa-e3b07-default-rtdb.europe-west1.firebasedatabase.app/");
+        reference = db.getReference().child("Users");
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        uID = currentUser.getUid();
+        Log.d(TAG, uID);
     }
 
 
