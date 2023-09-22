1f962d214faa7d584aab801153b303fb433e91f0
raz1359 <razcraft1359@gmail.com>
2023-09-23 02:50:45 +0300

    Finish image in circle
    need to FIX full name,Nickname

--- app/src/main/java/com/example/myapplication/FillProfile.java
+++ app/src/main/java/com/example/myapplication/FillProfile.java
@@ -11,6 +11,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.nfc.Tag;
 import android.os.Bundle;
@@ -25,7 +31,6 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
@@ -38,17 +43,19 @@ import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
-
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.UUID;
 import java.util.prefs.Preferences;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class FillProfile extends AppCompatActivity implements View.OnClickListener {
 
-    public static final  String NameLast_text = "" , NicknameLast_name = "";
+    public final  String NameLast_text = "" , NicknameLast_name = "";
 
-    private static final String TAG = "raz";
+    private final String TAG = "raz";
     public Button button;
     public ImageView imageView, profilePic;
     public Uri imageUri;
@@ -87,7 +94,6 @@ public class FillProfile extends AppCompatActivity implements View.OnClickListen
 
 
         button.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
 
@@ -115,12 +121,10 @@ public class FillProfile extends AppCompatActivity implements View.OnClickListen
             @Override
             public void afterTextChanged(Editable editable) {
 
-                pref1.edit().putString(NameLast_text , editable.toString()).commit();
+                pref1.edit().putString(NameLast_text , fullName.getText().toString()).commit();
             }
 
         });
-
-
         nickname.setText(pref2.getString(NicknameLast_name, ""));
         nickname.addTextChangedListener(new TextWatcher() {
             @Override
@@ -135,21 +139,15 @@ public class FillProfile extends AppCompatActivity implements View.OnClickListen
 
             @Override
             public void afterTextChanged(Editable editable) {
+                pref2.edit().putString(NicknameLast_name , nickname.getText().toString()).commit();
 
-                pref2.edit().putString(NicknameLast_name , editable.toString()).commit();
             }
         });
 
         storage = FirebaseStorage.getInstance();
         storageReference = storage.getReference();
 
-        profilePic.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                choosePicture();
-            }
-        });
+        profilePic.setOnClickListener(view -> choosePicture());
 
         // Firebase initi
         mAuth = FirebaseAuth.getInstance();
@@ -166,12 +164,16 @@ public class FillProfile extends AppCompatActivity implements View.OnClickListen
         startActivityForResult(intent,1);
     }
 
+    FirebaseDatabase db = FirebaseDatabase.getInstance();
+    DatabaseReference root = db.getReference();
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null){
             imageUri = data.getData();
-            profilePic.setImageURI(imageUri);
+            profilePic.setImageBitmap(getCircularBitmap(decodeUri(imageUri)));
             uploadPicture();
         }
     }
@@ -195,7 +197,6 @@ public class FillProfile extends AppCompatActivity implements View.OnClickListen
                         riverRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                             @Override
                             public void onSuccess(Uri uri) {
-                                Picasso.get().load(uri).resize(370,370).centerCrop().into(profilePic);
                                 datebaseReference.child("users").child(mAuth.getCurrentUser().getUid()).child("ProfileImage").setValue(uri.toString());
                             }
                         });
@@ -222,7 +223,37 @@ public class FillProfile extends AppCompatActivity implements View.OnClickListen
 
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
 
+    private Bitmap getCircularBitmap(Bitmap bitmap) {
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
 
+        int diameter = Math.min(width, height);
+        Bitmap output = Bitmap.createBitmap(diameter, diameter, Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(output);
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, diameter, diameter);
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawCircle(diameter / 2, diameter / 2, diameter / 2, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        return output;
+    }
+
+    private Bitmap decodeUri(android.net.Uri uri) {
+        try {
+            // Decode the image from the URI using BitmapFactory
+            return BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
